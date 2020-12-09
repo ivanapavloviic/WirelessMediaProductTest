915382c909beec863ac7c636f680a9e5b551944a
Ivana <ivanapavlovic1303@gmail.com>
2020-12-09 23:51:36 +0100

    [Created project]

---
+++ WirelessMediaProduct/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WirelessMediaProduct.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
